6dce9f872e32022d5bd809493f629877ed8d174c
BrianHadianSTEI23 <bri.hadian@gmail.com>
2025-07-25 18:15:19 +0700

    fix : hold up web3

--- backend/src/libs/web3.ts
+++ backend/src/libs/web3.ts
@@ -1,8 +1,8 @@
-import { ethers } from 'ethers';
-import ArtifactRegistry from '../../../blockchain/artifacts/contracts/ArtifactsRegistry.sol/ArtifactRegistry.json';
+// import { ethers } from 'ethers';
+// import ArtifactRegistry from '../../../blockchain/artifacts/contracts/ArtifactsRegistry.sol/ArtifactRegistry.json';
 
-const provider = new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/...");
-const signer = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
-const contract = new ethers.Contract("deployed_contract_address", ArtifactRegistry.abi, signer);
+// const provider = new ethers.JsonRpcProvider("https://sepolia.infura.io/v3/...");
+// const signer = new ethers.Wallet(process.env.PRIVATE_KEY!, provider);
+// const contract = new ethers.Contract("deployed_contract_address", ArtifactRegistry.abi, signer);
 
-export { contract };
+// export { contract };
